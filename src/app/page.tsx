3bc6e60177438178c7aa7820a315b2b9db022e4d
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-09-09 16:33:19 -0400

    Relocated coming-soon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import ComingSoon from '../components/coming-soon';
+import ComingSoon from './_components/coming-soon';
 
 export default function Home() {
   return (
